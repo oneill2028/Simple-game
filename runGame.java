d743d95a0b246fd18b065035ef70665ff432f3c4
oneill2028 <kevinguitar014@gmail.com>
2025-02-25 02:18:50 -0500

    Update runGame.java

--- runGame.java
+++ runGame.java
@@ -2,9 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class runGame {
-  public static String pleasure(String[] args) {
-    System.out.print("How are you")
+  public static String pleasure() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("How are you");
     String response = in.nextLine();
+    in.close();
     return response;
   }
 }
